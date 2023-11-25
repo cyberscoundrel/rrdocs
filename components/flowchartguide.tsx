d33d31647a0be0d4c08dcfbc19b021a0316251c9
cyberscoundrel <cyberscoundrel@gmail.com>
2023-11-25 14:30:19 -0600

    Merge branch 'dev2exp' into tmp

---
+++ components/flowchartguide.tsx
@@ -0,0 +1,10 @@
+import React, { Component, useEffect } from 'react'
+
+function FlowChartGuide(props) {
+    return(
+        <div>
+            <div id='guide-content'>{}</div>
+            <div id='guide-buttons'></div>
+        </div>
+    )
+}
